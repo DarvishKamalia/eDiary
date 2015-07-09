f6e25ecc26e164ed63af7f8359d0183d259dbad5
DarvishKamalia <darvish.kamalia@gmail.com>
2015-07-09 12:41:00 +0530

    started JSONmodule

--- swag.py
+++ swag.py
@@ -1,17 +1,35 @@
-# from flask import Flask
-# app = Flask(__name__)
-#
-# @app.route('/')
-# def hello_world():
-#     return 'Hello World!'
-#
-# if __name__ == '__main__':
-#     app.run()
+import json #JSON Encoder and Decoder library
 
-import json
+## JSONmodule
+#  This class controls file IO and stores entries in a map
+class JSONmodule:
 
+        ##The constructor
+        #@param filename A file containg the entries in valud JSON
+        # Loads the data from the json file into a dictionary
+        def __init__(self, filename = "test"):
+            updateMap (filename)
 
-r = open ('test', 'r')
-swag = json.load (r)
-r.close
-print swag["May 2015"][0]["Date"]
+        ##Updates the map by reading values from the given file
+        #@param filename
+        def updateMap (filename = "test"):
+            r = open (filename, 'r')
+            self.entryMap = json.load(r)
+            r.close()
+
+        ##Writes the dictionary to the file specified
+        def write (filename = "testout"):
+            w = open (filename, 'w')
+            json.load(self.entryMap, r)
+            w.close
+
+        ##Returns an array of entries for the given month
+        #@param month The month for which entries are requested
+        #@retval entries The entries for the specified month as an array
+        def getEntriesByMonth (month = "May 2015"):
+            return self.entryMap[month]
+
+        ##Inserts a new entry in the dictionary and writes the updated database to the given file
+        #@param newEntry A tuple containing the timestamp and the text of the new entry
+        #@param filename The name of the file where the database will be written after the update
+        def addEntry (newEntry = (""))
