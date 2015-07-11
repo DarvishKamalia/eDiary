2aa9210a85f2f3ede4e8a8befc1721299ff14bfa
Parth Mehta <pcm.seven@gmail.com>
2015-07-11 10:44:21 +0530

    search and info UI done, basic functional search function also done

--- parth/db.py
+++ parth/db.py
@@ -1,53 +1,16 @@
-from datetime import datetime
 import json
 
 file_db = 'storage/entries.json'
 
-def makeEntry(text):
-	now = datetime.now()
-	ampm = 'pm' if now.hour >= 12 else 'am'
-	hour = now.hour
-	if hour >= 13:
-		hour -= 12
-	if hour == 0:
-		hour = 12
-
-	months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-	
-	entry = {}
-	entry['id'] = '%d%d%d%d%d%d%d' % (now.year, now.month, now.day, now.hour, now.minute, now.second, now.microsecond)
-	entry['date'] = '%d %s \'%s' % (now.day, months[now.month - 1], str(now.year)[2:])
-	entry['time'] = '%d:%02d %s' % (hour, now.minute, ampm)
-	entry['text'] = text
-	return entry
-
-def getEntries():
+# load everything from file_db
+def load():
 	r = open(file_db, 'r')
 	entries = json.load(r)
 	r.close()
 	return entries
 
-def logEntry(entry):
-	r = open(file_db, 'r')
-	entries = json.load(r)
-	r.close()
-	entries.insert(0, entry)
-	w = open(file_db, 'w')
-	json.dump(entries, w)
-	w.close()
-
-def eraseEntry(id):
-	r = open(file_db, 'r')
-	entries = json.load(r)
-	r.close()
-
-	i = 0
-	while i < len(entries):
-		if id == entries[i]['id']:
-			del entries[i]
-			break
-		i += 1
-
+# store everything to file_db
+def store(entries):
 	w = open(file_db, 'w')
 	json.dump(entries, w)
 	w.close()
