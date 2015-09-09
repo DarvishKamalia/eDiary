3c91ed9e4542243d1462abe6314d359fa6775a1e
Parth Mehta <pcm.seven@gmail.com>
2015-09-09 19:06:02 +0530

    dskjsfnvkjnfd

---
+++ day4/temp.py
@@ -0,0 +1,7 @@
+# variables, types, values
+
+x = 0, 1, 2, 3, -5, -8 # int
+y = 'parth', 'mehta', 'python', 'binal mam is the best' # string
+z = 3.14, -0.04, 1.0000000 # float
+a = True, False # boolean
+
